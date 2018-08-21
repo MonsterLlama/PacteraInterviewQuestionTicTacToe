71c5393d46dfba39529543e381313af046c4a321
MonsterLlama <MonsterLlama@gmail.com>
2018-08-21 14:46:01 -0700

    Added new Unit Tests as well as refactored existing ones to address code bloat.

--- PacteraInterviewQuestion-TicTacToe/Program.cs
+++ PacteraInterviewQuestion-TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,11 @@ namespace PacteraInterviewQuestion_TicTacToe
 
         public TicTacToe(int rows)
         {
+            if (rows < 1)
+            {
+                throw new ArgumentOutOfRangeException(message: $"Can't play with a board size of {rows} x {rows}!", paramName: nameof(rows));
+            }
+
             _board = new Player?[rows, rows];
         }
 
@@ -31,7 +37,7 @@ namespace PacteraInterviewQuestion_TicTacToe
             // Validate the spot is within the Tic-Tac-Toe board.
             if (point.Row > _board.GetUpperBound(0) || point.Column > _board.GetUpperBound(1))
             {
-                throw new ArgumentOutOfRangeException(message: $"Can't play a spot outside the board! (Row: {point.Row}, Column: {point.Column})", paramName: nameof(point));
+                throw new MoveOutsideBoardException(message: $"Can't play a spot outside the board! (Row: {point.Row}, Column: {point.Column})", paramName: nameof(point));
             }
 
             // Validate the spot in the Tic-Tac-Toe board is available (current value is null).
@@ -89,7 +95,8 @@ namespace PacteraInterviewQuestion_TicTacToe
 
             // Check for a diagonal win. (left to right).. Eg., [0,n] [1,n-1] [2,n-2] ... [n,0]
             // Check if we're on the diagonal line from [0,n] to [n,0]
-            if (point.Row == _board.GetUpperBound(0) - point.Column)
+            // Test if row + column = rows
+            if (point.Row + point.Column == _board.GetUpperBound(0))
             {
                 for (int row = 0; row < _board.GetLength(0); row++)
                 {
@@ -107,7 +114,6 @@ namespace PacteraInterviewQuestion_TicTacToe
             return false;
         }
 
-
     }
 
     public enum Player { X, O };
@@ -124,5 +130,85 @@ namespace PacteraInterviewQuestion_TicTacToe
         }
 
     }
+
+    //
+    // Summary:
+    //     The exception that is thrown when the value of an argument is outside the allowable
+    //     range of values as defined by the invoked method.
+    public class MoveOutsideBoardException : ArgumentOutOfRangeException
+    {
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class.
+        public MoveOutsideBoardException() : base() { }
+
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class with
+        //     the name of the parameter that causes this exception and a specified error message.
+        //
+        // Parameters:
+        //   paramName:
+        //     The name of the parameter that caused the exception.
+        //
+        //   message:
+        //     The message that describes the error.
+        public MoveOutsideBoardException(string message, string paramName) : base(message, paramName) { }
+
+
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class with
+        //     the name of the parameter that causes this exception.
+        //
+        // Parameters:
+        //   paramName:
+        //     The name of the parameter that causes this exception.
+        public MoveOutsideBoardException(string paramName) : base(paramName) { }
+
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class with
+        //     a specified error message and the exception that is the cause of this exception.
+        //
+        // Parameters:
+        //   message:
+        //     The error message that explains the reason for this exception.
+        //
+        //   innerException:
+        //     The exception that is the cause of the current exception, or a null reference
+        //     (Nothing in Visual Basic) if no inner exception is specified.
+        public MoveOutsideBoardException(string message, Exception innerException) : base(message, innerException) { }
+
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class with
+        //     the parameter name, the value of the argument, and a specified error message.
+        //
+        // Parameters:
+        //   paramName:
+        //     The name of the parameter that caused the exception.
+        //
+        //   actualValue:
+        //     The value of the argument that causes this exception.
+        //
+        //   message:
+        //     The message that describes the error.
+        public MoveOutsideBoardException(string paramName, object actualValue, string message) : base(paramName, actualValue, message) { }
+
+        //
+        // Summary:
+        //     Initializes a new instance of the PacteraInterviewQuestion_TicTacToe.MoveOutsideBoardException class with
+        //     serialized data.
+        //
+        // Parameters:
+        //   info:
+        //     The object that holds the serialized object data.
+        //
+        //   context:
+        //     An object that describes the source or destination of the serialized data.
+        protected MoveOutsideBoardException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
+
 
 }
--- TicTacToeUnitTests/UnitTest1.cs
+++ TicTacToeUnitTests/UnitTest1.cs
@@ -5,71 +5,56 @@ using PacteraInterviewQuestion_TicTacToe;
 namespace TicTacToeUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class TicTacToeUnitTests
     {
         [TestMethod]
         public void TestVerticalWin()
         {
-            int size = 3; // 3x3
-
-            var board = new TicTacToe(size);
-
-            Player p1 = Player.X;
-            //Player p2 = Player.O;
+            var board = new TicTacToe(rows: 3);
 
             bool result = default(bool);
 
-            result = board.Move(point: new Point(row: 1, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 1, column: 1), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 1), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 0, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 0, column: 1), player: Player.X);
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void TestHorizontalWin()
         {
-            int size = 3; // 3x3
-
-            var board = new TicTacToe(size);
-
-            Player p1 = Player.X;
-            //Player p2 = Player.O;
+            var board = new TicTacToe(rows: 3);
 
             bool result = default(bool);
 
-            result = board.Move(point: new Point(row: 2, column: 2), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 2), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 1), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 0), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 0), player: Player.X);
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void TestHorizontalNoneWin()
         {
-            int size = 3; // 3x3
-
-            var board = new TicTacToe(size);
-
-            Player p1 = Player.X;
-            Player p2 = Player.O;
+            var board = new TicTacToe(rows: 3);
 
             bool result = default(bool);
 
-            result = board.Move(point: new Point(row: 2, column: 2), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 2), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 1), player: p2);
+            result = board.Move(point: new Point(row: 2, column: 1), player: Player.O);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 0), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 0), player: Player.X);
             Assert.IsFalse(result);
         }
 
@@ -77,45 +62,95 @@ namespace TicTacToeUnitTests
         [TestMethod]
         public void TestDiagonalWinLeftToRight()
         {
-            int size = 3; // 3x3
-
-            var board = new TicTacToe(size);
-
-            Player p1 = Player.X;
-            //Player p2 = Player.O;
+            var board = new TicTacToe(rows: 3);
 
             bool result = default(bool);
 
-            result = board.Move(point: new Point(row: 0, column: 0), player: p1);
+            result = board.Move(point: new Point(row: 0, column: 0), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 2), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 2), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 1, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 1, column: 1), player: Player.X);
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void TestDiagonalWinRightToLeft()
         {
-            int size = 3; // 3x3
-
-            var board = new TicTacToe(size);
-
-            Player p1 = Player.X;
-            //Player p2 = Player.O;
+            var board = new TicTacToe(rows: 3);
 
             bool result = default(bool);
 
-            result = board.Move(point: new Point(row: 0, column: 2), player: p1);
+            result = board.Move(point: new Point(row: 0, column: 2), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 2, column: 0), player: p1);
+            result = board.Move(point: new Point(row: 2, column: 0), player: Player.X);
             Assert.IsFalse(result);
 
-            result = board.Move(point: new Point(row: 1, column: 1), player: p1);
+            result = board.Move(point: new Point(row: 1, column: 1), player: Player.X);
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestIllegalBoardSize()
+        {
+            var board = new TicTacToe(rows: 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MoveOutsideBoardException))]
+        public void TestIllegalMoveOutsideBoard()
+        {
+            var board = new TicTacToe(rows: 3);
+
+            board.Move(point: new Point(row: 4, column: 4), player: Player.X);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIllegalMovePreexistingMoveAtSameSpotOnBoard()
+        {
+            var board = new TicTacToe(rows: 3);
+
+            board.Move(point: new Point(row: 1, column: 1), player: Player.X);
+            board.Move(point: new Point(row: 1, column: 1), player: Player.O);
+        }
+
+        [TestMethod]
+        public void TestResettingBoard()
+        {
+            // There isn't a great test for this.
+            // The individual spots on the board are not exposed outside
+            // its class. One approach, fill up all spots, reset board,
+            // attempt to refill up all spots. If a spot isn't cleared by
+            // the call to ResetBoard() then an Exception should be thrown.
+            // This assumes that that Exception mechanism works.
+            // We have a unit test for the mechanism elsewhere in this class.
+            // So it's possible for this test to fail but show as passing
+            // if that Exception mechanism isn't working.
+
+            var board = new TicTacToe(rows: 3);
+
+            board.Move(point: new Point(row: 0, column: 0), player: Player.X);      //  X   O   X
+            board.Move(point: new Point(row: 0, column: 1), player: Player.O);
+            board.Move(point: new Point(row: 0, column: 2), player: Player.X);
+  
+            // Validate the Exception Mechanism. Again, this is far from an idea or preferred test practice
+            // as this is testing different functionality than the purpose of this specific test's target: ResetBoard()
+            // If the Exception Mechanism isn't working, we'd expect to see its test as well as this test to fail.
+            Assert.ThrowsException<ArgumentException>(() => board.Move(point: new Point(row: 0, column: 0), player: Player.X));
+            Assert.ThrowsException<ArgumentException>(() => board.Move(point: new Point(row: 0, column: 1), player: Player.O));
+            Assert.ThrowsException<ArgumentException>(() => board.Move(point: new Point(row: 0, column: 2), player: Player.X));
+
+            board.ResetBoard();
+
+            board.Move(point: new Point(row: 0, column: 0), player: Player.X);      //  X   O   X
+            board.Move(point: new Point(row: 0, column: 1), player: Player.O);
+            board.Move(point: new Point(row: 0, column: 2), player: Player.X);
+        }
+
     }
 }
